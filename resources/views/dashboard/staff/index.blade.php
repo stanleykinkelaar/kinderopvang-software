722e8c9bc9d3b7b6f7a6563b04e4bff66cd7cc11
Stanley Kinkelaar <stanley@librus.nl>
2022-02-06 22:53:29 +0100

    idk what I did

--- resources/views/dashboard/staff/index.blade.php
+++ resources/views/dashboard/staff/index.blade.php
@@ -3,12 +3,7 @@
 @section('subtitle', 'Dashboard')
 
 @section('content')
-    <x-row cols="1">
 
-        <x-card width="1">
-            Staff dinges
-        </x-card>
 
-    </x-row>
 
 @endsection
